bf9e1ae8283f6f321952b6e12e4c46d684554e65
Cole Martindale <cole.martindale13@gmail.com>
2022-04-23 08:20:16 -0700

    Finish HTML structure

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const button1 = document.getElementById('button1');
     const button2 = document.getElementById('button2');
-    const button3 = document.getElementById('button3');
+    const audioCtx = new AudioContext();
     console.log(audioCtx);
     let audio1 = new Audio()
     let audio2 = new Audio();
@@ -18,41 +18,5 @@ document.addEventListener("DOMContentLoaded", function () {
     })
 
 
-    const container = document.getElementById('container');
-    const canvas = document.getElementById('canvas1');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    const ctx = canvas.getContext('2d');
-    let audioSource;
-    let analyzer;
-
-    container.addEventListener('click', function(){
-        let audio1 = new Audio();
-        const audioCtx = new AudioContext();
-        audio1.play();
-        audioSource = audioContext.createMediaElementSource(audio1);
-        analyzer = audioContext.createAnalyzer();
-        audioSource.connect(analyzer);
-        analyzer.connect(audioCtx.destination);
-        analyzer.fftSize = 64;
-        const bufferLength = analyzer.frequencyBinCount;
-        const dataArray = new Uint8Array(bufferLength);
-
-        const barWidth = canvas.width/bufferLength;
-        let barHeight;
-        let x = 0;
-
-        function animate(){
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            analyzer.getByteFrequencyData(dataArray);
-            for (let i = 0; i < bufferLength; i++) {
-                barHeight = dataArray[i];
-                ctx.fillStyle = 'white';
-                ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);                
-                x += barWidth
-            }
-            requestAnimationFrame(animate);
-        }
-        animate();
-    })
+    
 })
