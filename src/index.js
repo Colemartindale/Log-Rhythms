757a738806c529323d8d9d686bb743b9bf50c7bf
Cole Martindale <cole.martindale13@gmail.com>
2022-04-23 18:47:25 -0700

    Edit CSS and HTML

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@
 document.addEventListener("DOMContentLoaded", function () {
     const button1 = document.getElementById('button1');
     const button2 = document.getElementById('button2');
+    const button3 = document.getElementById('button3');
     const audioCtx = new AudioContext();
     console.log(audioCtx);
-    let audio1 = new Audio()
+    let audio1 = new Audio();
     let audio2 = new Audio();
+    let audio3 = new Audio();
      
     audio1.src = '../assets/Fox/HIYAAA.wav';
     audio2.src = '../assets/ROY/up-b.wav';
+    audio3.src = '../assets/okay.wav';
     button1.addEventListener('click', function(){
         audio1.play()
     })
     button2.addEventListener('click', function(){
         audio2.play()
+    })
+    button3.addEventListener('click', function(){
+        audio3.play()
     })
 
 
