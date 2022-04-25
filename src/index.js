bd2ace334e040d0525c5e9f5c5baac1f18003eb6
Cole Martindale <cole.martindale13@gmail.com>
2022-04-25 11:47:42 -0700

    Finish slider and dropdown HTML and CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
-
 document.addEventListener("DOMContentLoaded", function () {
+
     const button1 = document.getElementById('button1');
     const button2 = document.getElementById('button2');
     const button3 = document.getElementById('button3');
@@ -10,9 +10,9 @@ document.addEventListener("DOMContentLoaded", function () {
     let audio2 = new Audio();
     let audio3 = new Audio();
      
-    audio1.src = '../assets/Fox/HIYAAA.wav';
-    audio2.src = '../assets/ROY/up-b.wav';
-    audio3.src = '../assets/okay.wav';
+    audio1.src = '../assets/sounds/Fox/HIYAAA.wav';
+    audio2.src = '../assets/sounds/ROY/up-b.wav';
+    audio3.src = '../assets/sounds/okay.wav';
     button1.addEventListener('click', function(){
         audio1.play()
     })
@@ -22,7 +22,20 @@ document.addEventListener("DOMContentLoaded", function () {
     button3.addEventListener('click', function(){
         audio3.play()
     })
+   
+
+    const slider = document.getElementById('slider');
+    const currentTempo = document.querySelector('.tempo span');
+
+    slider.oninput = (() => {
+        let val = slider.value;
+        if (val < 100) {
+            currentTempo.innerHTML = "0" + val + " BPM"
+        } else {
+            currentTempo.innerHTML = val + " BPM"
+        }
+    });
 
 
-    
+  
 })
---
+++ src/scripts/tempo-slider.js
@@ -0,0 +1,8 @@
+// let currentTempo = document.querySelector('span')
+// let sliderValue = document.getElementsByClassName('slider-input')
+
+// sliderValue.addEventListener (() => {
+//     let currentValue = sliderValue.value;
+//     currentTempo.textContent = currentValue;
+// });
+
